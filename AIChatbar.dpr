4d85a0922cf47c85d7f9dc11a4d4c56f94373aa1
vhanla <vhanla@gmail.com>
2023-07-08 10:14:40 -0500

    Add project file

---
+++ AIChatbar.dpr
@@ -0,0 +1,38 @@
+program AIChatbar;
+
+
+
+{$R *.dres}
+
+uses
+  FMX.Forms,
+  Vcl.Forms,
+  Windows,
+  menu in 'menu.pas' {frmMenu},
+  Splash in 'Splash.pas',
+  settings in 'settings.pas' {frmSetting},
+  functions in 'functions.pas',
+  Vcl.Themes,
+  Vcl.Styles,
+  frmChatWebView in 'frmChatWebView.pas' {mainBrowser},
+  uBrowserCard in 'uBrowserCard.pas',
+  uBrowserFrame in 'uBrowserFrame.pas' {BrowserFrame: TFrame},
+  uChildForm in 'uChildForm.pas',
+  settingsHelper in 'settingsHelper.pas';
+
+{$R *.res}
+
+begin
+  if FindWindow('Win8MenuCLS', nil) > 0 then
+    Exit;
+
+  Application.Initialize;
+  Application.MainFormOnTaskBar := False;
+  TStyleManager.TrySetStyle('Windows11 Polar Dark');
+  Application.Title := 'AIChat';
+  Application.CreateForm(TfrmMenu, frmMenu);
+  Application.CreateForm(TfrmSetting, frmSetting);
+  Application.CreateForm(TmainBrowser, mainBrowser);
+  Application.Run;
+
+end.
