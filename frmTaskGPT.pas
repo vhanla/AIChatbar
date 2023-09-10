e0040740754de08b18c4ccb055c6b17a457f1d80
vhanla <vhanla@gmail.com>
2023-09-09 23:15:37 -0500

    🧩Add Queries to ChatGPT API endpoints

--- frmTaskGPT.pas
+++ frmTaskGPT.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.WinXCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.WinXCtrls,
+  System.Net.HttpClient;
 
 type
   TtaskForm = class(TForm)
@@ -15,8 +16,15 @@ type
     Label3: TLabel;
     Label4: TLabel;
     Button1: TButton;
+    ToggleSwitch1: TToggleSwitch;
+    Label5: TLabel;
     procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
+    FBearer: string;
+    FCookies: TCookieManager;
+    FUserAgent: string;
     { Private declarations }
   public
     { Public declarations }
@@ -24,6 +32,10 @@ type
     function RestoreCustomInstructions: Boolean;
     function AskGPT(const query: string): Boolean;
     function GetAccessToken: string;
+
+    property Bearer: string read FBearer write FBearer;
+    property Cookies: TCookieManager read FCookies write FCookies;
+    property UserAgent: string read FUserAgent write FUserAgent;
   end;
 
 var
@@ -34,7 +46,7 @@ implementation
 {$R *.dfm}
 
 uses
-  Net.HttpClient, System.JSON;
+  System.JSON, frmChatWebView;
 
 const
   //GET (to obtain current) or POST, and should return 200 with Authorization: Bearer
@@ -82,19 +94,67 @@ begin
   end;
 end;
 
+procedure TtaskForm.FormCreate(Sender: TObject);
+begin
+  FUserAgent := 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.0.0';
+  FCookies := TCookieManager.Create;
+end;
+
+procedure TtaskForm.FormDestroy(Sender: TObject);
+begin
+  FCookies.Destroy;
+end;
+
 function TtaskForm.GetAccessToken: string;
 var
   http: THTTPClient;
   resp: IHTTPResponse;
+  data: TStringStream;
 begin
+  Result := '';
   http := THTTPClient.Create;
+  data := TStringStream.Create;
   try
-    resp := http.Get('');
+    http.UserAgent := 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0';
+    //http.CookieManager := frmChatWebView.mainBrowser.GetGPTCookies;
+    http.ConnectionTimeout := 3000; // 3 seconds
+    http.CustomHeaders['Accept'] := '*/*';
+    http.CustomHeaders['Accept-Encoding'] := 'gzip, deflate, br';
+    http.CustomHeaders['Cache-Control'] := 'no-cache';
+    http.CustomHeaders['Connection'] := 'keep-alive';
+    http.CustomHeaders['Host'] := 'chat.openai.com';
+    http.CustomHeaders['Pragma'] := 'no-cache';
+    http.CustomHeaders['Sec-Fetch-Dest'] := 'emtpy';
+    http.CustomHeaders['Sec-Fetch-Mode'] := 'cors';
+    http.CustomHeaders['Sec-Fetch-Site'] := 'same-origin';
+
+    resp := http.Get('https://chat.openai.com/api/auth/session', data);
     if resp.StatusCode = 200 then
     begin
-      resp.ContentStream;
-    end;
+      var json := TJSONObject.Create;
+      try
+        if json.Parse(data.Bytes, 0) > 0 then // valid json
+        begin
+          if json.FindValue('accessToken') <> nil then
+          begin
+            Result := json.Values['accessToken'].Value;
+          end;
+        end;
+
+      finally
+        json.Free;
+      end;
+
+    end
+    else if resp.StatusCode = 403 then
+    begin
+      ShowMessage('You need to pass the Cloudflare protection. Please open the ChatGPT instance.');
+    end
+
+    else
+      raise Exception.Create(PChar('Error trying to get the access token, you should login first to ChatGPT:'#13#10#13#10 + data.ToString));
   finally
+    data.Free;
     http.Free;
   end;
 end;
@@ -105,8 +165,49 @@ begin
 end;
 
 function TtaskForm.SwitchCustomInstructions: Boolean;
+var
+  http: THTTPClient;
+  resp: IHTTPResponse;
+  data: TStringStream;
 begin
+  Result := False;
+  http := THTTPClient.Create;
+  data := TStringStream.Create;
+  try
+    http.UserAgent := 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0';
+    http.CookieManager := frmChatWebView.mainBrowser.GetGPTCookies;
+    http.ConnectionTimeout := 3000; // 3 seconds
+    http.CustomHeaders['Authorization'] := 'Bearer'+FBearer;
 
+    resp := http.Get('https://chat.openai.com/backend-api/user_system_messages', data);
+    if resp.StatusCode = 200 then
+    begin
+      var json := TJSONObject.Create;
+      try
+        if json.Parse(data.Bytes, 0) > 0 then // valid json
+        begin
+          if json.FindValue('about_user_message') <> nil then
+          begin
+            ShowMessage(json.Values['about_user_message'].Value);
+          end;
+        end;
+
+      finally
+        json.Free;
+      end;
+
+    end
+    else if resp.StatusCode = 403 then
+    begin
+      ShowMessage('You need to pass the Cloudflare protection. Please open the ChatGPT instance.');
+    end
+
+    else
+      raise Exception.Create(PChar('Error trying to get the access token, you should login first to ChatGPT:'#13#10#13#10 + data.ToString));
+  finally
+    data.Free;
+    http.Free;
+  end;
 end;
 
 end.
